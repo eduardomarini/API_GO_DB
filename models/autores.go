b1c519f57005e6f28ee3d7b26f211e84ef8260ba
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2024-11-12 10:25:05 -0300

    FIX: alteração tipo modelsAutores Data_nascimento

--- models/autores.go
+++ models/autores.go
@@ -1,8 +1,10 @@
 package models
 
+import "time"
+
 type Autores struct {
-	IDAutor         uint   `json:"ID_autor" gorm:"column:id;primaryKey"`
-	Nome            string `json:"Nome" gorm:"column:nome"`
-	Nacionalidade   string `json:"Nacionalidade" gorm:"column:nacionalidade"`
-	Data_nascimento string `json:"Data_nascimento" gorm:"column:data_nascimento"`
+	IDAutor         uint       `json:"ID_autor" gorm:"column:id;primaryKey"`
+	Nome            string     `json:"Nome" gorm:"column:nome"`
+	Nacionalidade   string     `json:"Nacionalidade" gorm:"column:nacionalidade"`
+	Data_nascimento *time.Time `json:"Data_nascimento" gorm:"column:data_nascimento"`
 }
